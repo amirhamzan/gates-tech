e6f6a5cc6e2ac8cd1b9f9d6ccb5973be34827486
amirhamzan <amirhamzan30@gmail.com>
2026-06-19 11:17:16 +0800

    Fix mistakenly compare the condition for PercentageDiscount and FixedDiscount in DiscountService

--- app/Services/DiscountService.php
+++ app/Services/DiscountService.php
@@ -21,16 +21,18 @@ class DiscountService
     {
         if ($this->strategy instanceof PercentageDiscount) {
             $percentage = $this->strategy->calculate($price);
-            if ($percentage < 0 || $percentage > 100) {
+            if ($percentage < 0 || $percentage > $price) {
                 throw new HttpException(400, 'Percentage discount, must between 0-100.');
             }
+            return $percentage;
         }
 
         if ($this->strategy instanceof FixedDiscount) {
             $amount = $this->strategy->calculate($price);
-            if ($amount < $price) {
+            if ($amount < 0) {
                 throw new HttpException(400, 'Fixed discount value, must not more than the original value.');
             }
+            return $amount;
         }
 
         return $this->strategy->calculate($price);
